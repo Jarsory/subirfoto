43b3c418b01e2f7aaa0d9239d106947ffb636b65
Jarsory <arsory92@gmail.com>
2019-07-20 11:52:25 -0500

    proyecto subir fotos

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router  = express.Router();
+const multer  = require('multer');
+const Picture = require('../models/picture');
+const upload = multer({ dest: './public/uploads/' });
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+
+  const pic = new Picture({
+    name: req.body.name,
+    tipoArchivo: req.file.mimetype,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
+});
+
+module.exports = router;
